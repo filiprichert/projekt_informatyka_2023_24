ce13516ab2e9f4f92e11032895fe39c4d21f7395
Filip <filipu@example.com>
2024-01-02 19:30:26 +0100

    szybkiezmiany

---
+++ Project1/Project1/MainMenu.cpp
@@ -0,0 +1,15 @@
+#include "MainMenu.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML/Audio.hpp>
+#include <math.h>
+#include <cstdlib>
+#include <vector>
+
+
+
+
+
---
+++ Project1/Project1/MainMenu.h
@@ -0,0 +1,15 @@
+//#pragma once
+//#include <SFML/Graphics.hpp>
+//#include <iostream>
+//
+//
+//#define Max_main_menu 4
+//class MainMenu
+//{
+//
+//public:
+//	MainMenu(float width, float height);
+//
+//
+//};
+//
